92e6a78f68e14d5d9c820c9788c80d99e82cae03
Flu3nt <gnedyalkov94@gmail.com>
2024-01-19 11:41:55 +0200

    rewrite recursive bs

--- src/__tests__/binary_search.test.ts
+++ src/__tests__/binary_search.test.ts
@@ -1,4 +1,4 @@
-import binary_search from "../algorithms/binary_search.ts";
+import { binary_search } from "../algorithms/binary_search.ts";
 
 test("binarySearch", function () {
     const sorted_arr: number[] = [2, 4, 6, 8, 10, 12, 14, 16, 18, 21, 23, 25, 27, 29, 30];
--- src/algorithms/binary_search.ts
+++ src/algorithms/binary_search.ts
@@ -1,4 +1,4 @@
-export default function binary_search(haystack: number[], needle: number): boolean {
+export function binary_search(haystack: number[], needle: number): boolean {
     let lo = 0;
     let hi = haystack.length;
 
@@ -6,9 +6,9 @@ export default function binary_search(haystack: number[], needle: number): boole
         let m = Math.floor(lo + (hi - lo) / 2);
         let v = haystack[m];
 
-        if (needle === m) {
+        if (needle === v) {
             return true;
-        } else if (needle > m) {
+        } else if (needle > v) {
             lo = m + 1;
         } else {
             hi = m;
